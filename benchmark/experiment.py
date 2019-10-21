4660303083686c5c0c7a1d5670a4e8c419d0468f
Alex Coca <alexcoca23@yahoo.co.uk>
2019-10-21 17:08:46 +0100

    feat: replaced json parser with yaml parser

--- benchmark/experiment.py
+++ benchmark/experiment.py
@@ -1,10 +1,9 @@
 import argparse
-import ast
-import json
 import os
 import pickle
 import sys
 import numpy as np
+import yaml
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.compose import ColumnTransformer
@@ -98,8 +97,8 @@ def predict_fcn(clf, preprocessor=None):
 
 def fit_rf(splits, config, preprocessor=None):
 
-    np.random.seed(int(config['seed']))
-    clf = RandomForestClassifier(n_estimators=int(config['n_estimators']))
+    np.random.seed(config['seed'])
+    clf = RandomForestClassifier(n_estimators=config['n_estimators'])
     clf.fit(preprocessor.transform(splits['X_train']), splits['Y_train'])
 
     display_performance(splits, predict_fcn(clf, preprocessor=preprocessor))
@@ -113,8 +112,8 @@ def get_tabular_explainer(predict_fn, dataset, split, config):
     category_map = dataset.category_map
     X_train = split['X_train']
 
-    explainer = AnchorTabular(predict_fn, feature_names, categorical_names=category_map, seed=int(config['seed']))
-    explainer.fit(X_train, disc_perc=ast.literal_eval(config['disc_perc']))
+    explainer = AnchorTabular(predict_fn, feature_names, categorical_names=category_map, seed=config['seed'])
+    explainer.fit(X_train, disc_perc=config['disc_perc'])
 
     return explainer
 
@@ -126,12 +125,12 @@ def display_prediction(predict_fn, instance_id, splits, dataset):
 
 
 def get_explanation(explainer, expln_config, splits, exp_config):
-    instance_id = int(exp_config['instance_idx'])
+    instance_id = exp_config['instance_idx']
 
     return explainer.explain(splits['X_test'][instance_id],
-                             threshold=float(expln_config['threshold']),
-                             verbose=ast.literal_eval(expln_config['verbose']),
-                             parallel=ast.literal_eval(expln_config['parallel']),
+                             threshold=expln_config['threshold'],
+                             verbose=expln_config['verbose'],
+                             parallel=expln_config['parallel'],
                              )
 
 
@@ -143,12 +142,12 @@ def display_explanation(explanation):
 
 class ExplainerExperiment(object):
 
-    def __init__(self, *, dataset, expln_config, clf_config, exp_config):
+    def __init__(self, *, dataset, explainer, classifier, experiment):
 
         self.dataset = dataset
-        self.explainer_config = expln_config
-        self.experiment_config = exp_config
-        self.clf_config = clf_config
+        self.explainer_config = explainer
+        self.experiment_config = experiment
+        self.clf_config = classifier
 
         self._this_module = sys.modules[__name__]
         # TODO: Implement _create_data_store
@@ -212,11 +211,11 @@ class ExplainerExperiment(object):
         self._data_store['t_elapsed'].append(t_elapsed)
 
 
-def run_experiment(args):
+def run_experiment(configuration):
 
-    n_runs = int(args['exp_config']['n_runs'])
+    n_runs = int(configuration['experiment']['n_runs'])
 
-    with ExplainerExperiment(**args) as exp:
+    with ExplainerExperiment(**configuration) as exp:
         for _ in range(n_runs):
             with Timer() as time:
                 explanation = get_explanation(exp.explainer,
@@ -225,50 +224,29 @@ def run_experiment(args):
                                               exp.experiment_config,
                                               )
             exp.update(explanation, time.t_elapsed)
-            if ast.literal_eval(args['exp_config']['verbose']):
+            if configuration['experiment']['verbose']:
                 display_explanation(explanation)
     return
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Anchor Explanations Experiments')
-    parser.add_argument('dataset',
+    parser.add_argument("--config",
+                        nargs="?",
                         type=str,
-                        default='adult',
-                        help='Dataset from which to select instance to explain.'
-                             'Allowed vals: adult/imagenet/movie_sentiment',
+                        default="configs/config.yaml",
+                        help="Configuration file for the experiment",
                         )
-    # TODO: Could have a config file containing these jsons (or something that generates them) and just
-    #  specify the file instead - here we'd just load the config
-    parser.add_argument('-clf-config',
-                        type=json.loads,
-                        help=r'JSON with classifier config. Example usage:'
-                             r'\'{"name": "rf", "seed": "0", "n_estimators": "5"}\'.'
-                             r'Must contain seed value. Only random forest currently supported'
-                             r' (name attribute must be rf).',
-                        )
-    parser.add_argument('-expln-config',
-                        type=json.loads,
-                        help=r'JSON with explainer config. Example usage:'
-                             r'\'{"type":"tabular", "threshold": "0.95", "parallel":"False", "verbose":"False",'
-                             r'"disc_perc":"(25, 50, 75)","seed":0"}\'.'
-                             r'"type" should be set to "tabular", "image" or "text"',
-                        )
-    parser.add_argument('-exp-config',
-                        type=json.loads,
-                        help=r'JSON with experiment config. Example usage:'
-                             r'\'{"n_runs": "100", "instance_idx": "6", "ckpt_dir":"/home/alex/my_exp",'
-                             r'"ckpt": "awesome.pkl","verbose":"False"}\'.'
-                             r'This specifies where to save the data and under what name, which test instance '
-                             r'should be explained',
-                        )
-    configuration = parser.parse_args()
+    args = parser.parse_args()
 
-    if configuration.expln_config['type'] not in SUPPORTED_EXPLAINERS:
+    with open(args.config) as fp:
+        configuration = yaml.load(fp, Loader=yaml.FullLoader)
+
+    if configuration['explainer']['type'] not in SUPPORTED_EXPLAINERS:
         raise NotImplementedError("Experiments are supported only for tabular data!")
-    if configuration.dataset not in SUPPORTED_DATASETS:
+    if configuration['dataset'] not in SUPPORTED_DATASETS:
         raise ValueError("Only datasets adult/imagenet/movie_sentiment are supported")
-    if configuration.clf_config['name'] not in SUPPORTED_CLASSIFIERS:
+    if configuration['classifier']['name'] not in SUPPORTED_CLASSIFIERS:
         raise NotImplementedError("Only random forest classifiers are supported!")
 
-    run_experiment(vars(configuration))
+    run_experiment(configuration)
